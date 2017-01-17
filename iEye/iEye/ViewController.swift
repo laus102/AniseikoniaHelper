1b7507d3f97950b0f97c56c5f59ee82f54b5138a
Brendan Lau <bjlau@umich.edu>
2017-01-17 16:48:25 -0500

    Initial

---
+++ iEye/iEye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iEye
+//
+//  Created by Brendan Lau on 1/17/17.
+//  Copyright © 2017 Incipia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
